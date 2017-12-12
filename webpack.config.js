1c9458fa0cab6ece68f5237083c481d710fa0ea8
Marcos Cassiano <marcoscssno@gmail.com>
2017-12-12 12:45:33 -0300

    Fixed some problems

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 var ENTRY_FILE = path.resolve(__dirname, 'src/server/index.js');
 var NODE_MODULES_FOLDER = path.resolve(__dirname, 'node_modules');
-var DIST_FOLDER = path.resolve(__dirname, 'dist/server');
+var DIST_FOLDER = path.resolve(__dirname, 'dist');
+var OUTPUT_FOLDER = path.resolve(__dirname, 'dist/server');
 
 module.exports = {
 
@@ -30,15 +31,15 @@ module.exports = {
             }
         ]
     },
-
+    
     plugins: [
         new CleanWebpackPlugin(DIST_FOLDER),
         new UglifyJsPlugin()
     ],
 
     output: {
-        path: DIST_FOLDER,
+        path: OUTPUT_FOLDER,
         filename: 'index.js'
     }
 
-}
+};
