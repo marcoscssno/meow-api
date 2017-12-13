682fd9b58a469364fe878312d862ae26d051acd2
Marcos Cassiano <marcoscssno@gmail.com>
2017-12-13 14:07:33 -0300

    Working with NODE_ENV variable

--- src/server/index.js
+++ src/server/index.js
@@ -9,10 +9,12 @@ import webpack from 'webpack'
 import config from '../../webpack.config.dev'
 import webpackDevMiddleware from 'webpack-dev-middleware'
 import webpackHotMiddleware from 'webpack-hot-middleware'
-const compiler = webpack(config)
 
-app.use(webpackDevMiddleware(compiler, {publicPath: config.output.publicPath}))
-app.use(webpackHotMiddleware(compiler))
+if(process.env.NODE_ENV === 'development') {
+  const compiler = webpack(config)
+  app.use(webpackDevMiddleware(compiler, {publicPath: config.output.publicPath}))
+  app.use(webpackHotMiddleware(compiler)) 
+}
 
 const html = `
 <!doctype html>
